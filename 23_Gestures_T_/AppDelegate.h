6d24d9722bb8029ad4475c845d3c00b326f2b97f
Stanly Shiyanovskiy <st-small@rambler.ru>
2016-02-15 10:21:16 +0200

    Initial Commit

---
+++ 23_Gestures_T_/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  23_Gestures_T_
+//
+//  Created by Stas on 15.02.16.
+//  Copyright © 2016 Stas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 23_Gestures_T_/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  23_Gestures_T_
+//
+//  Created by Stas on 15.02.16.
+//  Copyright © 2016 Stas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
